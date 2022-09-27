90cb629384e1267d61e91a7d7d0bf89acdf75db5
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-09-26 21:35:25 -0300

    estudo sobre react Router

--- matheusBattisti/meu/src/App.js
+++ matheusBattisti/meu/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Condicional from './components/Condicionais';
+// import Condicional from './components/Condicionais';
 // import HelloWorld from './components/HelloWorld';
 // import SayMyName from './components/sayMyName';
 // import Pessoa from './components/Pessoa';
@@ -8,13 +8,26 @@ import Condicional from './components/Condicionais';
 // import Evento from './components/Evento';
 // import Form from './components/Form';
 
+//import OutraLista from "./components/OutraLista";
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 function App() {
   return (
-    <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/empresa" element={<Empresa />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
