84a3e2cd9b406ca5a8f117ba2df8f25ad639c9a4
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-09-27 22:13:09 -0300

    inicio do projeto

---
+++ matheusBattisti/costs/src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/contact'>Contato</Link>
+          <Link to='/company'>Empresa</Link>
+          <Link to='/newproject'>Novo Projeto</Link>
+        </div>
+        <Container>
+        <Routes>
+            <Route exact path='/' element={<Home />}  />
+            <Route exact path='/Company' element={<Company />} />
+            <Route exact path='/Contact' element={<Contact />} />
+            <Route exact path='/Newproject' element={<NewProject />} />
+        </Routes>
+        </Container>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
