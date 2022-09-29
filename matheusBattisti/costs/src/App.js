765a657bfe1ce39f36da3478e6dde93bef821707
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-09-29 19:47:38 -0300

    criação da nav e footer

--- matheusBattisti/costs/src/App.js
+++ matheusBattisti/costs/src/App.js
@@ -1,30 +1,29 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Projects from './components/pages/Projects';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/contact'>Contato</Link>
-          <Link to='/company'>Empresa</Link>
-          <Link to='/newproject'>Novo Projeto</Link>
-        </div>
+      <Navbar />
         <Container customClass='min-height'>
         <Routes>
             <Route exact path='/' element={<Home />}  />
             <Route exact path='/Company' element={<Company />} />
+            <Route exact path='/Projects' element={<Projects />} />
             <Route exact path='/Contact' element={<Contact />} />
             <Route exact path='/Newproject' element={<NewProject />} />
         </Routes>
         </Container>
-        <p>Footer</p>
+        <Footer />
       </Router>
     </div>
   );
