5a71a816207a4dec07ae2b006d8f925ff4ce5d3f
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-11-16 21:42:46 -0300

    Context api

--- matheusBattisti/9_contextApi/context/src/main.jsx
+++ matheusBattisti/9_contextApi/context/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {CounterContextProvider} from './context/CounterContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </React.StrictMode>
 )
